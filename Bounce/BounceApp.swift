d75d500961a4cdab7d1e367878c46dc38f6ac2d5
omkar <salunkheomkar107@gmail.com>
2024-04-19 11:35:16 +0530

    Initial Commit

---
+++ Bounce/BounceApp.swift
@@ -0,0 +1,17 @@
+//
+//  BounceApp.swift
+//  Bounce
+//
+//  Created by mini project on 19/04/24.
+//
+
+import SwiftUI
+
+@main
+struct BounceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
